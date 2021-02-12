edcbe690f6475ae8a7ee9d85cae8c262832326ed
Romain Lebreton <romain.lebreton@lirmm.fr>
2021-02-12 17:18:59 +0100

    Fusionne 2 tests redondants

--- src/test/java/fr/umontpellier/iut/CompteTest.java
+++ src/test/java/fr/umontpellier/iut/CompteTest.java
@@ -26,14 +26,7 @@ class CompteTest {
 
     @Disabled
     @Test
-    public void test_creer_offre_prix_trop_grand_par_rapport_au_solde(){
-        OffreEnchere o = compte.creerOffre(produit,500,500);
-        assertNull(o);
-    }
-
-    @Disabled
-    @Test
-    public void test_creer_offre_solde_insuffisant(){
+    public void test_creer_offre_solde_insuffisant_pour_prix_max_plus_cout_participation(){
         OffreEnchere o = compte.creerOffre(produit,50,100);
         assertNull(o);
     }
