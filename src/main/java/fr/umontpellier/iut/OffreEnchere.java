0f5b7d6c740dd0f166f1beda683320532f30e0eb
Petru Valicov <petru.valicov@etu.umontpellier.fr>
2022-02-10 10:41:06 +0100

    refactoring

--- src/main/java/fr/umontpellier/iut/OffreEnchere.java
+++ src/main/java/fr/umontpellier/iut/OffreEnchere.java
@@ -1,11 +1,6 @@
 package fr.umontpellier.iut;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-
 public class OffreEnchere {
-    private LocalDate date;
-    private LocalTime heure;
     private double prixEnCours;
     private double prixMax;
     private Produit produit;
@@ -16,8 +11,6 @@ public class OffreEnchere {
         this.prixEnCours = prixEnCours;
         this.prixMax = prixMax;
         this.produit = produit;
-        date = LocalDate.now();
-        heure = LocalTime.now();
         etatGagnant = false;
         this.monCompte = monCompte;
     }
@@ -37,8 +30,6 @@ public class OffreEnchere {
     @Override
     public String toString() {
         return "OffreEnchere{" +
-                "date=" + date +
-                ", heure=" + heure +
                 ", prixEnCours=" + prixEnCours +
                 ", prixMax=" + prixMax +
                 ", produit=" + produit.getNumero() +
